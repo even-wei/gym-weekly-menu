5f273c854b843f4ba4f09bf25d038dce04958e3c
Even Wei <evenwei@infuseai.io>
2022-02-03 20:48:43 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'weekly-menu',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
